34ef4e1d70a3cd4dfcac2617d6db4330ebfc19e5
acbelter <acbelter@gmail.com>
2014-01-07 20:49:30 +0300

    Added copyright.

--- src/com/acbelter/makesumgame/activity/GameActivity.java
+++ src/com/acbelter/makesumgame/activity/GameActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 acbelter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.acbelter.makesumgame.activity;
 
 import android.app.Activity;
@@ -131,6 +147,7 @@ public class GameActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        //setFieldEmpty(false);
         if (mCurrentTimerState != null) {
             mTimer = new BlinkedCountDownTimer(mTimerView,
                     mCurrentTimerState.millsUntilFinished, 10*1000, mTimerHandler);
@@ -248,13 +265,44 @@ public class GameActivity extends Activity {
         }
     }
 
+/*    private void setFieldEmpty(boolean empty) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            for (int j = 0; j < FIELD_SIZE; j++) {
+                if (empty) {
+                    mFieldButtons[i][j].setText("");
+                } else {
+                    mFieldButtons[i][j].setText(mGameState.getFieldValue(i, j));
+                }
+            }
+        }
+    }*/
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mTimer != null) {
+            mTimer.cancel();
+        }
+    }
+
+/*    @Override
+    protected void onPause() {
+        super.onPause();
+        setFieldEmpty(true);
+    }*/
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        //setFieldEmpty(true);
         outState.putParcelable(BaseGameState.KEY_GAME_STATE, mGameState);
         outState.putBoolean(KEY_GAME_OVER_FLAG, mGameOverFlag);
+        mCurrentTimerState = mTimer.getCurrentState();
         if (!mGameOverFlag) {
-            outState.putParcelable(KEY_TIMER_STATE, mTimer.getCurrentState());
+            outState.putParcelable(KEY_TIMER_STATE, mCurrentTimerState);
+        }
+        if (mTimer != null) {
+            mTimer.cancel();
         }
     }
 }
--- src/com/acbelter/makesumgame/activity/HighscoresActivity.java
+++
@@ -1,6 +0,0 @@
-package com.acbelter.makesumgame.activity;
-
-import android.app.Activity;
-
-public class HighscoresActivity extends Activity {
-}
